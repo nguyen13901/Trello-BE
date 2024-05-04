f9547ac4ffd51a6067b77dc713cf60bed75b8e30
PhamThanhNguyen <nguyen13901@gmail.com>
2024-05-04 18:36:10 +0700

    Implement create new board API

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,7 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection (Name & Schema)
 const BOARD_COLLECTION_NAME = 'boards'
@@ -7,14 +10,43 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(50).trim().strict(),
 
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  // Lưu ý các item trong mảng columnOrderIds là ObjectId nên cần thêm pattern cho chuẩn nhé, (lúc quay video số 57 mình quên nhưng sang đầu video số 58 sẽ có nhắc lại về cái này.)
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (id) => {
+
+  try {
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: new ObjectId(id)
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,3 +1,4 @@
+import { boardModel } from '~/models/boardModel'
 import { slugify } from '~/utils/formatters'
 
 
@@ -10,12 +11,17 @@ const createNew = async (reqBody) => {
     }
 
     // Gọi tới Model để xử lý lưu bản ghi newBoard vào Database
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi mới được tạo rồi trả về (step này có thể có hoặc không tùy mục đích,
+    // ví dụ FE cần show dữ vừa được tạo thì hãy trả về)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
     // Làm thêm các xử lý logic khác với những phần liên quan đến board (Ex: Board owner, card, column....)
 
     // Bắn email, notification về cho admin khi có 1 cái board mới được tạo...
 
-    return newBoard
+    return getNewBoard
   } catch (error) {
     error
   }
