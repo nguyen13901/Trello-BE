cfd3d65fe0180bff7df82d1949fb83c427ad5391
PhamThanhNguyen <nguyen13901@gmail.com>
2024-05-04 14:33:05 +0700

    Set up routes and validations

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,15 @@
 
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoute } from '~/routes/v1/boardRoute'
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'Call API successfully' })
+})
+
+/* Boards APIs */
+Router.use('/boards', boardRoute)
+
+export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -3,24 +3,21 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CLOSE_DB, CONNECT_DB, GET_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
 
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
+  // Enable req.body json data
+  app.use(express.json())
 
-
-  app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    res.end('<h1>Hello World!</h1>')
-  })
+  // Use APIs V1
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
-
 
   // Thực hiện các tác vụ clean up trước khi đóng server
   exitHook(() => {
